8034dc3460d1f614cd2e45e18d7e00d02a448083
Susan Hern <42076232+SusanHern@users.noreply.github.com>
2018-08-06 15:16:36 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
-
-$dbhost = "us-cdbr-iron-east-04.cleardb.net";
-$dbname = "heroku_3336584ac2bc325";
-$dbuser = "b181847c700112";
-$dbpass = "5982e545";
+//no DB set up file as its automatic for online and offline settings
+error_reporting(0);
+$dbhost = "";
+$dbname = "";
+$dbuser = "";
+$dbpass = "";
   
          
          $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
    
 ?>
+
